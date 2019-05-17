b96c4deae6f737442c5a1dc22ccfc22e8c1a8b71
Meyer <ehlmeyer@gmail.com>
2019-05-17 18:43:39 -0500

    changing select sets local state to tag_id

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from '@material-ui/core'
+import './Tags.css'
 
 
 class Tags extends Component {
     //dispatch action to trigger fetchTags saga to GET tags from database
+    state = {
+        // image_id : 0,
+        tag_id : 0
+    }
+    
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_TAGS' })
     }
 
+
+    handleChange = (event) => {
+        console.log('in handleChange', event.target.value);
+        this.setState({
+            tag_id: event.target.value
+        })
+
+        
+    }
     render(){
         return(
-            
-            <select>
+            <>
+            <select onChange={this.handleChange}>
+                {/* default to select blank */}
+                <option selected disabled>Pick One</option>
                 {this.props.reduxState.tags.map((tag) => {
                     return(
-                        <option>{tag.name}</option>
+                        <option value={tag.id}>{tag.name}</option>
                     )
                 })}
-                
             </select>
+            </>
         )
     }
 }
