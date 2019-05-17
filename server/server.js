188d31d7c8abdeccd677130e0cd46b059dce25ca
Meyer <ehlmeyer@gmail.com>
2019-05-17 16:34:06 -0500

    setup server-side routers

--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const tagsRouter = require('./routes/tags.router');
+const imagesRouter = require('./routes/images.router');
+const addTagRouter = require('./routes/addtag.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/api/tags', tagsRouter);
+app.use('/api/images', imagesRouter);
+app.use('/api/images/addtag', addTagRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
