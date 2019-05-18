ca470b49bbde1509e27b54ef4e191a1582407eeb
Meyer <ehlmeyer@gmail.com>
2019-05-17 19:16:56 -0500

    addTag saga sends tag_id and image_id to server

--- server/routes/addtag.router.js
+++ server/routes/addtag.router.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
+router.post('/', (req,res)=>{
+    const tag_id = req.body.tag_id
+    const image_id = req.body.image_id
+    console.log('in POST /api/images/addtag. tag_id:', tag_id, 'image_id:', image_id);
+})
+
 module.exports = router;
